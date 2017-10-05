7a23db3e48a2b1261b95b6193fffbf69963be658
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2017-10-05 18:28:57 -0300

    Add https example

---
+++ src/https.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
+const url = require('url')
+
+const getFiles = require('./get-files')
+const dependenciesConfig = require('./dependencies.json')
+
+const key = fs.readFileSync(path.resolve(__dirname, '../ssl/key.pem'))
+const cert = fs.readFileSync(path.resolve(__dirname, '../ssl/cert.pem'))
+
+const publicDir = path.resolve(__dirname, '../public/')
+const publicFiles = getFiles(publicDir)
+
+const handler = (req, res) => {
+  const location = url.parse(req.url)
+  const reqPath = location.path === '/' ? '/index.html' : location.path
+  const file = publicFiles.get(reqPath)
+
+  if (!file) {
+    res.statusCode = 404
+    res.end()
+    return
+  }
+
+  res.writeHead(200, file.headers)
+  fs.createReadStream(path.join(publicDir, reqPath)).pipe(res)
+}
+
+const server = https.createServer({ cert, key }, handler)
+
+server.listen(3000, () => {
+  const { address, port } = server.address()
+  console.log(`Server listening at ${address}:${port}`)
+})
