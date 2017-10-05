615fbe832a16aa3b66b6a2b23e86d02f6f5e751d
Pablo Machado <machadoum@gmail.com>
2017-10-05 11:40:05 -0300

    Serve index.html on http-server

--- src/http.js
+++ src/http.js
@@ -2,8 +2,20 @@ const fs = require('fs')
 const http = require('http')
 const path = require('path')
 
-const handler = (req, res) => {
+const indexPath = path.resolve(__dirname, '../public/index.html')
 
+const handler = (req, res) => {
+  fs.readFile(indexPath, function(err, data) {
+    if (!data) {
+      res.writeHead(500)
+    } else {
+      res.setHeader('Content-Type', 'text/html')
+      res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length })
+      res.write(data)
+    }
+
+    res.end()
+  })
 }
 
 const server = http.createServer(handler)
